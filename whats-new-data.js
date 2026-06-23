de640643ddfdb85f309878603766ca07663ab7c0
adamgeyer <ajgeyer@ucdavis.edu>
2026-06-23 12:45:08 -0700

    Create whats-new-data.js

---
+++ whats-new-data.js
@@ -0,0 +1,8 @@
+const CHANGELOG = [
+  {
+    "title": "Added a What's New page",
+    "date": "June 23, 2026",
+    "description": "Added a What's New page so you can see updates to this site without me telling you directly.",
+    "prNumber": 1
+  }
+];
